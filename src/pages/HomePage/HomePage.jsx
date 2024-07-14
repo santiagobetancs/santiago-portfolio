ec47d9ba7e9ef4d083f48d21ec7fa12e85bb1f47
santiagobetancs <160289567+santiagobetancs@users.noreply.github.com>
2024-07-14 16:56:01 -0400

    hero styling for all three breakpoints

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
-import Footer from '../../components/Footer/Footer'
-import Header from '../../components/Header/Header'
-import './HomePage.scss'
+import './HomePage.scss';
+import Footer from '../../components/Footer/Footer';
+import Header from '../../components/Header/Header';
+import Hero from '../../components/Hero/Hero';
 
 export default function HomePage() {
     return (
         <>
             <Header />
+            <Hero />
             <Footer />
         </>
     )
