8b387d1b6df1b3c19225e2e6a020175d4d84a504
santiagobetancs <160289567+santiagobetancs@users.noreply.github.com>
2024-07-14 16:38:57 -0400

    footer-styling

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,33 @@
+import "./Footer.scss";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { IoIosMail } from "react-icons/io";
+import { Link } from "react-router-dom";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <main className="footer__container">
+        <h1 className="footer__header">SANTIAGO BETANCOURTH</h1>
+        <section className="footer__social">
+          <Link
+            className="footer__link"
+            to="https://github.com/santiagobetancs"
+          >
+            <FaGithub className="footer__media" />
+          </Link>
+          <Link
+            className="footer__link"
+            to="https://www.linkedin.com/in/santiago-betancourth/"
+          >
+            <FaLinkedin className="footer__media" />
+          </Link>
+          {/* no route here */}
+          <Link className="footer__link">
+            <IoIosMail className="footer__media" />
+          </Link>
+        </section>
+      </main>
+    </footer>
+  );
+}
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,4 @@
+import Footer from '../../components/Footer/Footer'
 import Header from '../../components/Header/Header'
 import './HomePage.scss'
 
@@ -5,6 +6,7 @@ export default function HomePage() {
     return (
         <>
             <Header />
+            <Footer />
         </>
     )
 }
