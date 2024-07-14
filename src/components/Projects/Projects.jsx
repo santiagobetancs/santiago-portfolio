c5c3838f915d5853bcfb1e302276fb2968791bec
santiagobetancs <160289567+santiagobetancs@users.noreply.github.com>
2024-07-14 19:51:23 -0400

    working on the tech stack

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,9 +1,17 @@
 import './Projects.scss';
+import Project from '../Project/Project';
 
 export default function Projects() {
     return (
         <main className='projects'>
-
+            <section className='projects__container projects__container--top'>
+                <h1 className='projects__title'>
+                    Projects
+                </h1>
+            </section>
+            <section className='projects__container projects__container--bottom'>
+                <Project />
+            </section>
         </main>
     )
 }
